14f6045f7a854aa02c36b3b55150d52c04b267e5
chaedir <chaedirmuaw@gmail.com>
2018-01-13 21:37:47 +0700

    Completion
    Udah beres copasnya coy wakakaka

--- Assets/PortalTextureSetup.cs
+++ Assets/PortalTextureSetup.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class PortalTextureSetup : MonoBehaviour {
 
+    public Camera camera1;
+    public Material cameraMat1;
+
     public Camera camera2;
     public Material cameraMat2;
 
 	// Use this for initialization
 	void Start () {
-		if(camera2.targetTexture != null)
+        if (camera1.targetTexture != null)
+        {
+            camera1.targetTexture.Release();
+        }
+        camera1.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMat1.mainTexture = camera1.targetTexture;
+
+        if (camera2.targetTexture != null)
         {
             camera2.targetTexture.Release();
         }
