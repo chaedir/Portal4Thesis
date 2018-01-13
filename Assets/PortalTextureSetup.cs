25502abe5802fa2f8d283e15dc03c1c5646cf416
chaedir <chaedirmuaw@gmail.com>
2018-01-13 09:04:14 +0700

    penyempurnaan portal textur
    textur yang ditangkap oleh portal diperbaiki lebih rapih lagi sehingga benar2 terlihat portal sungguhan.

---
+++ Assets/PortalTextureSetup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour {
+
+    public Camera camera2;
+    public Material cameraMat2;
+
+	// Use this for initialization
+	void Start () {
+		if(camera2.targetTexture != null)
+        {
+            camera2.targetTexture.Release();
+        }
+        camera2.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMat2.mainTexture = camera2.targetTexture;
+	}	
+	 
+}
