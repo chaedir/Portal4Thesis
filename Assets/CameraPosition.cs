9766d5f085de1aefdb2b36be7a9205ded44ec4ff
chaedir <chaedirmuaw@gmail.com>
2018-03-21 12:53:25 +0700

    terbaru

---
+++ Assets/CameraPosition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(GameObject.Find("Camera1").transform.position);
+	}
+}
